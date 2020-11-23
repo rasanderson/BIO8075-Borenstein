15e7689607a3d7ed01c9f830bf652ce002122901
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2020-11-23 10:56:21 +0000

    Set up as 4 columns for PROVEIT trial

--- Chap01_shiny/app.R
+++ Chap01_shiny/app.R
@@ -13,20 +13,35 @@ ui <- fluidPage(
     plotOutput("metaPlot"),
     
     fluidRow(
-        column(6,
-               sliderInput("ntPROVEIT",
+        column(3,
+               sliderInput("nt_1",
                            "nt for PROVE IT",
                            min = 1000,
                            max = 5000,
                            value = 2099)
-        ),
+               ),
         
-        column(6, 
-               sliderInput("ncPROVEIT",
+        column(3, 
+               sliderInput("nc_1",
                            "nc for PROVE IT",
                            min = 1000,
                            max = 5000,
-                           value = 2063), style = "display:inline-block")
+                           value = 2063)
+               ),
+        column(3,
+               sliderInput("ep1t_1",
+                           "ep1t for PROVEIT",
+                           min = 50,
+                           max = 250,
+                           value = 147)
+               ),
+        column(3,
+               sliderInput("ep1c_1",
+                           "ep1c for PROVEIT",
+                           min = 50,
+                           max = 250,
+                           value = 172)
+        )
     ),
     
     actionButton("reset", "Reset values")
@@ -37,14 +52,18 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     observeEvent(input$reset, {
         # Reset values back to original
-        updateSliderInput(session, "ntPROVEIT", value = 2099)
-        updateSliderInput(session, "ncPROVEIT", value = 2063)
+        updateSliderInput(session, "nt_1", value = 2099)
+        updateSliderInput(session, "nc_1", value = 2063)
+        updateSliderInput(session, "ep1t_1", value = 147)
+        updateSliderInput(session, "ep1c_1", value = 172)
     })
 
     output$metaPlot <- renderPlot({
         # Modify inputs as needed
-        dat[1, "nt"] <- input$ntPROVEIT
-        dat[1, "nc"] <- input$ncPROVEIT
+        dat[1, "nt"] <- input$nt_1
+        dat[1, "nc"] <- input$nc_1
+        dat[1, "ep1t"] <- input$ep1t_1
+        dat[1, "ep1c"] <- input$ep1c_1
 
         dat <- escalc(measure="RR", ai=ep1t, n1i=nt, ci=ep1c, n2i=nc, data=dat, slab=trial)
         res <- rma(yi, vi, data=dat, method="DL")
