cb9bfb539bbcc08bf81acac13e31d61cdb9df2d2
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2020-11-24 10:28:33 +0000

    Change to log Response Ratios

--- Chap01_shiny/app.R
+++ Chap01_shiny/app.R
@@ -17,34 +17,13 @@ ui <- fluidPage(
                h4("PROVE IT")
                ),
         
-        column(2,
-               sliderInput("nt_1",
+        column(8,
+               sliderInput("yi_1",
                            label = NULL, 
-                           min = 1000,
-                           max = 6000,
-                           value = 2099)
+                           min = 0.5,
+                           max = 1.5,
+                           value = 0.84)
                ),
-        column(2,
-               sliderInput("nc_1",
-                           label = NULL,
-                           min = 1000,
-                           max = 6000,
-                           value = 2063) #,
-               ),
-        column(2,
-               sliderInput("ep1t_1",
-                           label = NULL,
-                           min = 50,
-                           max = 750,
-                           value = 147),
-               ),
-        column(2,
-               sliderInput("ep1c_1",
-                           label = NULL,
-                           min = 50,
-                           max = 750,
-                           value = 172),
-               )
         ),
         
         fluidRow(
@@ -52,35 +31,13 @@ ui <- fluidPage(
                   h4("A-TO-Z")
             ),
 
-            column(2,
-                   sliderInput("nt_2",
+            column(8,
+                   sliderInput("yi_2",
                                label = NULL, 
-                               min = 1000,
-                               max = 6000,
-                               value = 2265)
+                               min = 0.5,
+                               max = 1.5,
+                               value = 0.860)
             ),
-            column(2,
-                   sliderInput("nc_2",
-                               label = NULL,
-                               min = 1000,
-                               max = 6000,
-                               value = 2232) #,
-            ),
-            column(2,
-                   sliderInput("ep1t_2",
-                               label = NULL,
-                               min = 50,
-                               max = 750,
-                               value = 205),
-            ),
-            column(2,
-                   sliderInput("ep1c_2",
-                               label = NULL,
-                               min = 50,
-                               max = 750,
-                               value = 235),
-            )
-
         ),
     
     fluidRow(
@@ -88,70 +45,28 @@ ui <- fluidPage(
                h4("TNT")
         ),
         
-        column(2,
-               sliderInput("nt_3",
+        column(8,
+               sliderInput("yi_3",
                            label = NULL, 
-                           min = 1000,
-                           max = 6000,
-                           value = 4995)
+                           min = 0.5,
+                           max = 1.5,
+                           value = 0.801)
         ),
-        column(2,
-               sliderInput("nc_3",
-                           label = NULL,
-                           min = 1000,
-                           max = 6000,
-                           value = 5006) #,
-        ),
-        column(2,
-               sliderInput("ep1t_3",
-                           label = NULL,
-                           min = 50,
-                           max = 750,
-                           value = 334),
-        ),
-        column(2,
-               sliderInput("ep1c_3",
-                           label = NULL,
-                           min = 50,
-                           max = 750,
-                           value = 418),
-        )
-        
+       
     ),
     fluidRow(
         column(4,
                h4("IDEAL")
         ),
         
-        column(2,
-               sliderInput("nt_4",
+        column(8,
+               sliderInput("yi_4",
                            label = NULL, 
-                           min = 1000,
-                           max = 6000,
-                           value = 4439)
+                           min = 0.5,
+                           max = 1.5,
+                           value = 0.890)
         ),
-        column(2,
-               sliderInput("nc_4",
-                           label = NULL,
-                           min = 1000,
-                           max = 6000,
-                           value = 4449) #,
-        ),
-        column(2,
-               sliderInput("ep1t_4",
-                           label = NULL,
-                           min = 50,
-                           max = 750,
-                           value = 411),
-        ),
-        column(2,
-               sliderInput("ep1c_4",
-                           label = NULL,
-                           min = 50,
-                           max = 750,
-                           value = 463),
-        )
-        
+      
     ),
     
     actionButton("reset", "Reset values")
@@ -162,45 +77,22 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     observeEvent(input$reset, {
         # Reset values back to original
-        updateSliderInput(session, "nt_1", value = 2099)
-        updateSliderInput(session, "nc_1", value = 2063)
-        updateSliderInput(session, "ep1t_1", value = 147)
-        updateSliderInput(session, "ep1c_1", value = 172)
-        updateSliderInput(session, "nt_2", value = 2265)
-        updateSliderInput(session, "nc_2", value = 2232)
-        updateSliderInput(session, "ep1t_2", value = 205)
-        updateSliderInput(session, "ep1c_2", value = 235)
-        updateSliderInput(session, "nt_3", value = 4995)
-        updateSliderInput(session, "nc_3", value = 5006)
-        updateSliderInput(session, "ep1t_3", value = 334)
-        updateSliderInput(session, "ep1c_3", value = 418)
-        updateSliderInput(session, "nt_4", value = 4439)
-        updateSliderInput(session, "nc_4", value = 4449)
-        updateSliderInput(session, "ep1t_4", value = 411)
-        updateSliderInput(session, "ep1c_4", value = 463)
-        
+        updateSliderInput(session, "yi_1", value = 0.840)
+        updateSliderInput(session, "yi_2", value = 0.860)
+        updateSliderInput(session, "yi_3", value = 0.801)
+        updateSliderInput(session, "yi_4", value = 0.880)
     })
 
     output$metaPlot <- renderPlot({
-        # Modify inputs as needed
-        dat[1, "nt"] <- input$nt_1
-        dat[1, "nc"] <- input$nc_1
-        dat[1, "ep1t"] <- input$ep1t_1
-        dat[1, "ep1c"] <- input$ep1c_1
-        dat[2, "nt"] <- input$nt_2
-        dat[2, "nc"] <- input$nc_2
-        dat[2, "ep1t"] <- input$ep1t_2
-        dat[2, "ep1c"] <- input$ep1c_2
-        dat[3, "nt"] <- input$nt_3
-        dat[3, "nc"] <- input$nc_3
-        dat[3, "ep1t"] <- input$ep1t_3
-        dat[3, "ep1c"] <- input$ep1c_3
-        dat[4, "nt"] <- input$nt_4
-        dat[4, "nc"] <- input$nc_4
-        dat[4, "ep1t"] <- input$ep1t_4
-        dat[4, "ep1c"] <- input$ep1c_4
-        
+
         dat <- escalc(measure="RR", ai=ep1t, n1i=nt, ci=ep1c, n2i=nc, data=dat, slab=trial)
+        
+        # Modify effect sizes (which are Risk Ratios so have to be logs)
+        dat[1, "yi"] <- log(input$yi_1)
+        dat[2, "yi"] <- log(input$yi_2)
+        dat[3, "yi"] <- log(input$yi_3)
+        dat[4, "yi"] <- log(input$yi_4)
+        
         res <- rma(yi, vi, data=dat, method="DL")
         dat$weights <- paste0(round(weights(res)), "%")   # weights in % (rounded)
         dat$pvals   <- round(summary(dat)$pval, digits=3) # p-values of the individual trials
